6366889478999bb46911d37b07274b45f535fed3
nichoth <nichoth@gmail.com>
2020-08-20 16:12:14 -0700

    stuff

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = require('./src')
+var Eventual = require('./src/view')
+
+function subscribe (bus, state) {
+    bus.on('foo', ev => state.foo.set('bar'))
+}
+
+var { bus, view, state } = Eventual()
+
+// here pass in an `app`
+subscribe(bus, state)
+
+Eventual.createElement(view, document.getElementById('content'))
