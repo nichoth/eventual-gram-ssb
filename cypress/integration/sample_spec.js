40143c2d0f44989c49abc965c0579c46280de9df
Nick Thomas <nichoth@gmail.com>
2021-01-02 16:26:12 -0800

    race condition in test

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -66,52 +66,53 @@ describe('a new post', () => {
 })
 
 // Need to follow the feed2 and see if the image shows on the home page
-// img needs to be a file like in the browser
 
-// describe('a second feed', () => {
-//     it('should publish', () => {
-//         cy.visit('/')
-//         console.log('env', process.env.NODE_ENV)
-//         console.log('sboooooot', _sbot)
-//         var alice = ssbKeys.generate()
-//         var feed = ssbFeed(_sbot, alice)
+describe('a second feed', () => {
+    it('should publish', () => {
+        // console.log('env', process.env.NODE_ENV)
+        console.log('sboooooot', _sbot)
+        var alice = ssbKeys.generate()
+        var feed = ssbFeed(_sbot, alice)
 
-//         console.log('**feed**', feed)
+        console.log('**feed**', feed)
 
-//         function publishAlice () {
-//             feed.publish({
-//                 type: ts.post,
-//                 text: 'hello world, I am alice.'
-//             }, function (err, res) {
-//                 console.log('**in here**', err, res)
+        publishAlice()
 
-//                 expect(err).to.not.exist
+        function publishAlice () {
+            () => cy.visit('/')
 
-//                 // check if msg 2 exists in feed 1
-//                 S(
-//                     _sbot.messagesByType({ type: ts.post }),
-//                     S.collect((err, msgs) => {
-//                         expect(err).to.not.exist
-//                         var post = msgs.find(msg => {
-//                             return msg.value.author === feed.id
-//                         })
-//                         expect(post).to.exist
-//                         expect(post.value.author).to.equal(alice.id)
-//                         expect(post.value.content.text).to.equal(
-//                             'hello world, I am alice.')
+            feed.publish({
+                type: ts.post,
+                text: 'hello world, I am alice.'
+            }, function (err, res) {
+                console.log('**in here**', err, res)
 
-//                         _sbot.close(function (end) {
-//                             var err = (end && end !== true)
-//                             expect(err).to.be.false
-//                         })
-//                     })
-//                 )
-//             })
-//         }
+                // check if msg 2 exists in feed 1
+                S(
+                    _sbot.messagesByType({ type: ts.post }),
+                    S.collect((err, msgs) => {
+                        expect(err).to.not.exist
+                        var post = msgs.find(msg => {
+                            return msg.value.author === feed.id
+                        })
+                        expect(post).to.exist
+                        expect(post.value.author).to.equal(alice.id)
+                        expect(post.value.content.text).to.equal(
+                            'hello world, I am alice.')
+                    })
+                )
+            })
+        }
+    })
 
-//         publishAlice()
-//     })
-// })
+    // after(() => {
+    //     // runs once after all tests in the block
+    //     _sbot.close(function (end) {
+    //         var err = (end && end !== true)
+    //         expect(err).to.be.false
+    //     })
+    // })
+})
 
 // describe('blurbur', () => {
 //     it('does the thing', () => {
